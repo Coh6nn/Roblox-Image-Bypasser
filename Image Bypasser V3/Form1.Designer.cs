8f5a6c427b71a22b84942e7b6ac3de9f66731d20
Juxstin <Juxstin>
2018-08-08 03:01:20 -0500

    First!
    First!

---
+++ Image Bypasser V3/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Image_Bypasser_V3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.monoFlat_ThemeContainer1 = new MonoFlat.MonoFlat_ThemeContainer();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.monoFlat_Button4 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Button3 = new MonoFlat.MonoFlat_Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.monoFlat_Button2 = new MonoFlat.MonoFlat_Button();
+            this.monoFlat_Button1 = new MonoFlat.MonoFlat_Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.monoFlat_Label1 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_Label2 = new MonoFlat.MonoFlat_Label();
+            this.monoFlat_ThemeContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monoFlat_ThemeContainer1
+            // 
+            this.monoFlat_ThemeContainer1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(41)))), ((int)(((byte)(50)))));
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Label1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.textBox2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button4);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button3);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.textBox1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button2);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.monoFlat_Button1);
+            this.monoFlat_ThemeContainer1.Controls.Add(this.pictureBox1);
+            this.monoFlat_ThemeContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monoFlat_ThemeContainer1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_ThemeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.monoFlat_ThemeContainer1.Name = "monoFlat_ThemeContainer1";
+            this.monoFlat_ThemeContainer1.Padding = new System.Windows.Forms.Padding(10, 70, 10, 9);
+            this.monoFlat_ThemeContainer1.RoundCorners = true;
+            this.monoFlat_ThemeContainer1.Sizable = true;
+            this.monoFlat_ThemeContainer1.Size = new System.Drawing.Size(388, 518);
+            this.monoFlat_ThemeContainer1.SmartBounds = true;
+            this.monoFlat_ThemeContainer1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation;
+            this.monoFlat_ThemeContainer1.TabIndex = 0;
+            this.monoFlat_ThemeContainer1.Text = "RGB Image Bypasser By SugarBæ#8746";
+            this.monoFlat_ThemeContainer1.Click += new System.EventHandler(this.monoFlat_ThemeContainer1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(390, 59);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(287, 431);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "2";
+            // 
+            // monoFlat_Button4
+            // 
+            this.monoFlat_Button4.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button4.Image = null;
+            this.monoFlat_Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button4.Location = new System.Drawing.Point(195, 455);
+            this.monoFlat_Button4.Name = "monoFlat_Button4";
+            this.monoFlat_Button4.Size = new System.Drawing.Size(189, 35);
+            this.monoFlat_Button4.TabIndex = 5;
+            this.monoFlat_Button4.Text = "Save Image";
+            this.monoFlat_Button4.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button4.Click += new System.EventHandler(this.monoFlat_Button4_Click);
+            // 
+            // monoFlat_Button3
+            // 
+            this.monoFlat_Button3.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button3.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button3.Image = null;
+            this.monoFlat_Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button3.Location = new System.Drawing.Point(0, 455);
+            this.monoFlat_Button3.Name = "monoFlat_Button3";
+            this.monoFlat_Button3.Size = new System.Drawing.Size(189, 35);
+            this.monoFlat_Button3.TabIndex = 4;
+            this.monoFlat_Button3.Text = "Copy To Clipboard";
+            this.monoFlat_Button3.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button3.Click += new System.EventHandler(this.monoFlat_Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 273);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 176);
+            this.textBox1.TabIndex = 3;
+            // 
+            // monoFlat_Button2
+            // 
+            this.monoFlat_Button2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button2.Image = null;
+            this.monoFlat_Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button2.Location = new System.Drawing.Point(195, 232);
+            this.monoFlat_Button2.Name = "monoFlat_Button2";
+            this.monoFlat_Button2.Size = new System.Drawing.Size(189, 35);
+            this.monoFlat_Button2.TabIndex = 2;
+            this.monoFlat_Button2.Text = "Convert";
+            this.monoFlat_Button2.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button2.Click += new System.EventHandler(this.monoFlat_Button2_Click);
+            // 
+            // monoFlat_Button1
+            // 
+            this.monoFlat_Button1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Button1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.monoFlat_Button1.Image = null;
+            this.monoFlat_Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.monoFlat_Button1.Location = new System.Drawing.Point(0, 232);
+            this.monoFlat_Button1.Name = "monoFlat_Button1";
+            this.monoFlat_Button1.Size = new System.Drawing.Size(189, 35);
+            this.monoFlat_Button1.TabIndex = 1;
+            this.monoFlat_Button1.Text = "Open Image";
+            this.monoFlat_Button1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.monoFlat_Button1.Click += new System.EventHandler(this.monoFlat_Button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 167);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // monoFlat_Label1
+            // 
+            this.monoFlat_Label1.AutoSize = true;
+            this.monoFlat_Label1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(125)))), ((int)(((byte)(132)))));
+            this.monoFlat_Label1.Location = new System.Drawing.Point(-3, 503);
+            this.monoFlat_Label1.Name = "monoFlat_Label1";
+            this.monoFlat_Label1.Size = new System.Drawing.Size(80, 15);
+            this.monoFlat_Label1.TabIndex = 7;
+            this.monoFlat_Label1.Text = "Version: V3 R3";
+            // 
+            // monoFlat_Label2
+            // 
+            this.monoFlat_Label2.AutoSize = true;
+            this.monoFlat_Label2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_Label2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.monoFlat_Label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(125)))), ((int)(((byte)(132)))));
+            this.monoFlat_Label2.Location = new System.Drawing.Point(207, 503);
+            this.monoFlat_Label2.Name = "monoFlat_Label2";
+            this.monoFlat_Label2.Size = new System.Drawing.Size(183, 15);
+            this.monoFlat_Label2.TabIndex = 8;
+            this.monoFlat_Label2.Text = "Changelog: Speed Improvements";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 518);
+            this.Controls.Add(this.monoFlat_ThemeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "RGB Image Bypasser By SugarBæ#8746";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.monoFlat_ThemeContainer1.ResumeLayout(false);
+            this.monoFlat_ThemeContainer1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_ThemeContainer monoFlat_ThemeContainer1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog dialog;
+        private MonoFlat.MonoFlat_Button monoFlat_Button4;
+        private MonoFlat.MonoFlat_Button monoFlat_Button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private MonoFlat.MonoFlat_Button monoFlat_Button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private MonoFlat.MonoFlat_Label monoFlat_Label2;
+        private MonoFlat.MonoFlat_Label monoFlat_Label1;
+    }
+}
+
---
+++ Image Bypasser V3/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Bypasser_V3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //MessageBox.Show("Version V3 R2 Speed Improvements!");
+            InitializeComponent();
+        }
+        
+        private void monoFlat_Button1_Click(object sender, EventArgs e)
+        {
+            this.dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;...";
+            this.dialog.Title = "Select a picture to convert.";
+            bool flag = this.dialog.ShowDialog() == DialogResult.OK;
+            if (flag)
+            {
+                this.pictureBox1.ImageLocation = this.dialog.FileName;
+            }
+        }
+        private string str = "";
+        private void monoFlat_Button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            this.str = "spawn(function() local imageBytes = {";
+            this.pictureBox1.BackColor = Color.Transparent;
+            Bitmap bitmap = new Bitmap(this.dialog.FileName);
+            int num = int.Parse(this.textBox2.Text);
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    bool flag = i % num == 0 && j % num == 0;
+                    if (flag)
+                    {
+                        Color pixel = bitmap.GetPixel(i, j);
+                        this.str = string.Concat(new object[]
+                        {
+                            this.str,
+                            "\n {x = ",
+                            i,
+                            ", y = ",
+                            j,
+                            ", r = ",
+                            pixel.R,
+                            ", g = ",
+                            pixel.G,
+                            " , b = ",
+                            pixel.B,
+                            "}, "
+                        });
+                    }
+                }
+            }
+            this.str = string.Concat(new object[]
+            {
+                this.str,
+                "}\r\nlocal X = ",
+                this.pictureBox1.Width,
+                " local Y = ",
+                this.pictureBox1.Height
+            });
+            this.str += " local P = Instance.new('Part')\r\nP.Name = 'Bitmap'\r\nP.Anchored = true\r\nP.Transparency = 1\r\nP.Position = Vector3.new(0, 0, 0)\r\nP.Parent = game.Workspace\r\n\r\nlocal B = Instance.new('BillboardGui')\r\nB.Name = 'Image'\r\nB.AlwaysOnTop = true\r\nB.Size = UDim2.new(0, X, 0, Y)\r\nB.Parent = P\r\n local F = Instance.new('Frame')\r\nF.Name = 'Holder'\r\nF.Size = UDim2.new(1, 0, 1, 0)\r\nF.BackgroundTransparency = 1\r\nF.Parent = B";
+            this.str = this.str + "\r\nlocal res = " + this.textBox2.Text;
+            this.str += " local current = 1\r\n\r\nlocal cooldown = false\r\n\r\nlocal cooldowncount = 0\r\n\r\nfor _,v in pairs(imageBytes) do\r\n\t spawn(function() local frame = Instance.new('Frame')\r\n\tframe.BackgroundColor3 = Color3.new(v.r/255, v.g/255, v.b/255)\r\n\tframe.Size = UDim2.new(0, 1, 0, 1)\r\n\tframe.BorderSizePixel = 0\r\n\tframe.Position = UDim2.new(0, math.floor(v.x/res), 0, math.floor(v.y/res))\r\n\tframe.Parent = F\r\n\tif frame.BackgroundColor3 == Color3.new(0, 0, 0) then\r\n\t\tframe:Destroy()\r\n\tend end)\r\n\tend end)";
+            this.textBox1.Text = this.str;
+        }
+
+        private void monoFlat_Button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            // set a default file name
+            savefile.FileName = "decal.lua";
+            // set filters - this can be done in properties as well
+            savefile.Filter = "Lua files (*.lua)|*.lua|All files (*.*)|*.*";
+
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(savefile.FileName))
+                    sw.Write(textBox1.Text);
+            }
+
+        }
+
+        private void monoFlat_Button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        private void monoFlat_ThemeContainer1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
